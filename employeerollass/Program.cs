d5ab2cf3bfdf6c6fd60934791f43d7e2b6feca23
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-28 15:23:44 +0530

    [roshan]welecome salary masage

---
+++ employeerollass/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace employeerollass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
